8eacbe9533ba2ea1edb712ebab6a203e086da69e
Kevin Siml <appzer@users.noreply.github.com>
2018-02-20 21:59:52 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# sentry-pushsafer
-Pushsafer.com notification module for Sentry
+.. image:: https://www.pushsafer.com/de/assets/logos/logo.png
+    :alt: Pushsafer.com
+    :width: 100%
+    :align: center
+	
+Sentry-Pushsafer
+=============
+A Sentry plugin that sends notifications to [Pushsafer](https://www.pushsafer.com) registered device(s).
+
+.. _Pushsafer Notification Service: https://www.pushsafer.com/ 
+.. __: https://www.pushsafer.com/en/pushapi
+
+Forked from and original created by: [dz0ny](https://github.com/dz0ny/sentry-pushover)
+
+Installation
+------------
+sentry-pushsafer is available via PyPI, the python package index.
+
+To install simple type `pip install https://github.com/appzer/sentry-pushsafer/archive/master.zip`.
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+'''
+Sentry-Pushsafer
+=============
+A [Sentry](https://www.getsentry.com/) plugin that sends notifications to [Pushsafer](https://www.pushsafer.com) registered device(s).
+
+License
+-------
+Copyright 2018 Appzer.de Kevin Siml
+
+This file is part of Sentry-Pushsafer.
+
+Sentry-Pushsafer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name='sentry-pushsafer',
+    version='1.0.0',
+    author='Kevin Siml',
+    author_email='support@pushsafer.com',
+    url='https://github.com/appzer/sentry-pushsafer',
+    description='A Sentry plugin that integrates with pushsafer',
+    long_description=__doc__,
+    license='GPL',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'sentry.plugins': [
+            'pushsafer = sentry_pushsafer.plugin:PushsaferNotifications'
+        ]
+    },
+    include_package_data=True,
+)
